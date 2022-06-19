c3872cfb4636c91d94cdc43831e8e4debb0c4bc2
Kartik-A-Patil <105985004+Kartik-A-Patil@users.noreply.github.com>
2022-06-19 17:46:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function linktohomepage() {
   function showTime(){
     var date = new Date();
     var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
+    var m = date.getMinutes();
     var session = "AM";
     
     if(h == 0){
@@ -46,7 +46,7 @@ function linktohomepage() {
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
     
-    setTimeout(showTime, 1000);
+    setTimeout(showTime, 60000);
     
 }
 showTime();
@@ -100,3 +100,26 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
